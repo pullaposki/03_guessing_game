3a16ce3457135a9128e13327889e1c12971bf9d0
Janne Soikkeli <pullaposki@windowslive.com>
2024-04-06 14:35:57 +0300

    added guess count

--- main.c
+++ main.c
@@ -1,26 +1,22 @@
-#include <stdlib.h>
 #include <stdio.h>
 
 int main() {
     int secret = 5;
-    int guess = 0;
-    int gameOver = 0;
+    int guess;
+    int guessLimit = 3;
 
-    while (gameOver == 0) {
+    while (guessLimit > 0) {
         printf("Guess the number: ");
         scanf("%d", &guess);
 
         if (guess == secret) {
             printf("You guessed it!\n");
-            gameOver = 1;
-        } else {
-            printf("Try again!\n");
-            if(guess > secret) {
-                printf("The secret number is lower than your guess.\n");
-            } else {
-                printf("The secret number is higher than your guess.\n");
-            }
+            return 0;
         }
+
+        guessLimit--;
+        printf("The secret number is %s than your guess. Try again!\n", guess > secret ? "lower" : "higher");
+        printf("You have %d guesses left.\n", guessLimit);
     }
 
     return 0;
